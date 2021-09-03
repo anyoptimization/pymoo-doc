479a4f2c981dd2a3c11ad07ad6f74ed57d6dc151
Julian Blank <blankjul@egr.msu.edu>
2021-09-03 11:28:09 -0400

    Workflow 5

--- source/conf.py
+++ source/conf.py
@@ -118,21 +118,21 @@ exclude_patterns = ['build', '**.ipynb_checkpoints']
 if DEBUG:
     # exclude_patterns.append("**ipynb")
     # exclude_patterns.append("getting_started*")
-    # exclude_patterns.append("interface*")
-    # exclude_patterns.append("problems*")
-    #
-    # exclude_patterns.append("problems/single/*")
-    # exclude_patterns.append("problems/multi/*")
-    # exclude_patterns.append("problems/many/*")
-    # exclude_patterns.append("problems/constrained/*")
-    #
-    # # exclude_patterns.append("algorithms*")
-    # exclude_patterns.append("customization*")
-    # exclude_patterns.append("operators*")
-    # exclude_patterns.append("visualization*")
-    # exclude_patterns.append("api*")
-    # exclude_patterns.append("decision_making*")
-    # exclude_patterns.append("misc*")
+    exclude_patterns.append("interface*")
+    exclude_patterns.append("problems*")
+
+    exclude_patterns.append("problems/single/*")
+    exclude_patterns.append("problems/multi/*")
+    exclude_patterns.append("problems/many/*")
+    exclude_patterns.append("problems/constrained/*")
+
+    exclude_patterns.append("algorithms*")
+    exclude_patterns.append("customization*")
+    exclude_patterns.append("operators*")
+    exclude_patterns.append("visualization*")
+    exclude_patterns.append("api*")
+    exclude_patterns.append("decision_making*")
+    exclude_patterns.append("misc*")
 
     pass
 
