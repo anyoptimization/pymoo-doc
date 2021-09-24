34a9809b1e6c0da79fde342d55708cc047d325cb
Julian Blank <jules89@arcor.de>
2021-09-24 08:45:29 -0400

    Addec '--overwrite' flag to only overwrite all notebooks when executed remotely

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+
+
+def pytest_addoption(parser):
+    parser.addoption("--overwrite", action="store_true")
--- tests/test_docs.py
+++ tests/test_docs.py
@@ -6,15 +6,14 @@ from util import files_from_folder, run_ipynb, ROOT
 
 DOCS = os.path.join(ROOT, "source")
 
-OVERWRITE = True
-
-SKIP = ["parallelization.ipynb", "video.ipynb", "modact.ipynb", "dascmop.ipynb"]
+SKIP = ["parallelization.ipynb", "video.ipynb", "modact.ipynb", "dascmop.ipynb", "constraints.ipynb"]
 
 IPYNBS = [e for e in files_from_folder(DOCS, regex='**/*.ipynb', skip=SKIP) if ".ipynb_checkpoints" not in e]
 
 
 @pytest.mark.parametrize('ipynb', IPYNBS)
-def test_docs(ipynb):
-    run_ipynb(ipynb, overwrite=OVERWRITE, remove_trailing_empty_cells=True)
+def test_docs(ipynb, pytestconfig):
+    overwrite = pytestconfig.getoption("overwrite")
+    run_ipynb(ipynb, overwrite=overwrite, remove_trailing_empty_cells=True)
     assert True
 
